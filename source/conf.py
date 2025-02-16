9ae5459cfb627d7d0b2e9274698146e0bc252e35
Jose <pepeisa.ortiz@gmail.com>
2025-02-16 12:11:56 -0500

    path _static

--- source/conf.py
+++ source/conf.py
@@ -25,5 +25,5 @@ language = 'es'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+html_static_path = ['docs/_static']
 
